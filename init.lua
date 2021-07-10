93f2a117b1ac5603481a8f2fe95781ec67dbd578
nathias <n@th1.as>
2021-07-10 20:03:18 +0200

    Change color theme

--- init.lua
+++ init.lua
@@ -6,3 +6,18 @@ require "options"
 if not ok then
    error("Error loading core" .. "\n\n" .. err)
 end
+local g = vim.g
+
+g.mapleader = " "
+g.auto_save = false
+
+-- colorscheme related stuff
+
+g.nths_theme = "nths"
+local base16 = require "base16"
+base16(base16.themes["onedark"], true)
+
+require "highlights"
+require "mappings"
+
+require("utils").hideStuff()
